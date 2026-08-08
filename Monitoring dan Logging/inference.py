e038f1692587b48fdb56a6a009f1dee202fba108
MuhammadAusid <ausidaddari123@gmail.com>
2026-08-08 19:53:34 +0700

    Update inference.py

--- Monitoring dan Logging/inference.py
+++ Monitoring dan Logging/inference.py
@@ -1,9 +1,11 @@
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import joblib
 import pandas as pd
 import os
+import time
+
+from prometheus_client import Counter, Histogram, Gauge, make_asgi_app
 
 app = FastAPI(
     title="Heart Disease Prediction API",
@@ -11,23 +13,37 @@ app = FastAPI(
     version="1.0.0"
 )
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+REQUEST_COUNTER = Counter('model_predictions_total', 'Total prediksi yang dilakukan')
 
+LATENCY_HISTOGRAM = Histogram('model_prediction_latency_seconds', 'Waktu latensi prediksi')
+
+POSITIVE_PREDICTION_GAUGE = Gauge('heart_disease_positive_predictions_total', 'Total prediksi positif penyakit jantung')
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    
+    # Hanya catat metrik jika endpoint-nya /predict
+    if request.url.path == "/predict":
+        REQUEST_COUNTER.inc()
+        LATENCY_HISTOGRAM.observe(process_time)
+        
+    return response
+
+
+metrics_app = make_asgi_app()
+app.mount("/metrics", metrics_app)
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.abspath(
-    os.path.join(
-        BASE_DIR,
-        "..",
-        "Membangun_model",
-        "saved_model",
-        "heart_model.pkl"
-    )
+    os.path.join(BASE_DIR, "..", "Membangun_model", "saved_model", "heart_model.pkl")
 )
 
-print("Model Path :", MODEL_PATH)
-print("Model Exists :", os.path.exists(MODEL_PATH))
-
 model = joblib.load(MODEL_PATH)
-
 
 class HeartData(BaseModel):
     age: float
@@ -44,21 +60,16 @@ class HeartData(BaseModel):
     ca: float
     thal: float
 
-
 @app.get("/")
 def home():
-    return {
-        "message": "Heart Disease Prediction API is running"
-    }
-
+    return {"message": "Heart Disease Prediction API is running"}
 
 @app.post("/predict")
 def predict(data: HeartData):
-
     df = pd.DataFrame([data.dict()])
-
     prediction = int(model.predict(df)[0])
+    
+    if prediction == 1:
+        POSITIVE_PREDICTION_GAUGE.inc()
 
-    return {
-        "prediction": prediction
-    }
+    return {"prediction": prediction}
